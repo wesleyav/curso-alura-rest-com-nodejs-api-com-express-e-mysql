eada757efe10fc3ce674e7225a4c5f5ee5f64025
Wesley <wesley.contato@hotmail.com>
2020-08-13 20:47:52 -0300

    Criando nossa base de dados com MySQL

--- projeto/index.js
+++ projeto/index.js
@@ -1,5 +1,13 @@
 const customExpress = require('./config/customExpress')
 
-const app = customExpress()
+const conexao = require('./infraestrutura/conexao')
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+conexao.connect(erro => {
+    if (erro) {
+        console.log(erro)
+    } else {
+        console.log('Conectado com sucesso!')
+        const app = customExpress()
+        app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+    }
+})
