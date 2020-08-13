f91620e721a6a3c441c71aefb08f6dffe8cc92af
Wesley <wesley.contato@hotmail.com>
2020-08-13 19:14:22 -0300

    Usando o nodemon

--- projeto/index.js
+++ projeto/index.js
@@ -4,4 +4,4 @@ const app = express()
 
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
 
-app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos!'))
+app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET!'))
